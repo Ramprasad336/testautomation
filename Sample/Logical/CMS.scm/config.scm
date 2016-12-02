e5b3be8a156324dcac29ea6c39fb6812b0c7fe19
Prashanth <Prashanth>
2016-12-02 21:45:21 +0200

    First commit first commit

---
+++ Sample/Logical/CMS.scm/config.scm
@@ -0,0 +1,3 @@
+<?xml version="1.0" encoding="UTF-8"?><editableResource createdTimestamp="1480623618566" createdUser="Prashanth" id="54b938cd:158bc0b1787:-7f42" type="service_component_resource" updatedTimestamp="1480623618566" updatedUser="Prashanth" version="1.1">
+   <loggingConfiguration debug="false" writeStubEventsToDB="false"/>
+</editableResource>
